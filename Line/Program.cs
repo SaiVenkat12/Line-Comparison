00d4c745633cb03e6602a2599e4e128d4db3ac80
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-03 21:13:34 +0530

    [SaiVenkat]-Length of the Line

--- Line/Program.cs
+++ Line/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Line
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter values for first line: ");
+            Console.WriteLine("Enter the Co-ordinates of x1,x2");
+
+            Console.Write("x1: ");
+            double x1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("x2: ");
+            double x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Co-ordinates of y1,y2");
+
+            Console.Write("y1: ");
+            double y1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("y2: ");
+            double y2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter values for second line: ");
+            Console.WriteLine("Enter the Co-ordinates of x3,x4");
+
+            Console.Write("x3: ");
+            double x3 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("x4: ");
+            double x4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Co-ordinates of y3,y4");
+
+            Console.Write("y3: ");
+            double y3 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("y4: ");
+            double y4 = Convert.ToInt32(Console.ReadLine());
+
+            double length1 = Math.Sqrt((Math.Pow((x2 - x1), 2)) + (Math.Pow((y2 - y1), 2)));
+            Console.WriteLine("The Lenth of Line 1 : " + length1);
+
+            double Length2 = Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
+            Console.WriteLine("The Length of line 2 : " + Length2);
+
+        }
+    }
+}
